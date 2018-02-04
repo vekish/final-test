7647e86bff50af4aa102a5593d17ec63695720d5
Vera  Sercel <sercelv@yahoo.com>
2018-02-04 13:44:28 +0100

    searchBar

---
+++ ComtradeGramFinal/Utilities/SearchTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SearchTableViewCell.swift
+//  ComtradeGramFinal
+//
+//  Created by Vera  Sercel on 2/4/18.
+//  Copyright © 2018 com.comtrade.Gram. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profilePhoto: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ComtradeGramFinal/ViewControllers/SearchViewController copy.swift
+++ ComtradeGramFinal/ViewControllers/SearchViewController copy.swift
@@ -7,17 +7,59 @@
 //
 
 import UIKit
+import SwiftInstagram
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    var instagramPosts : [InstagramMedia] = []
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       //return instagramPosts.count
+        return 5
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 300
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "cell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? SearchTableViewCell else {
+            fatalError("error")
+            
+        }
+        
+        cell.profilePhoto.layer.cornerRadius = (cell.profilePhoto.frame.width) / 2
+        
+        
+        
+        return cell
+        
     }
 
 }
